cda644e82e59818a968bb065712820bd4eeea516
Juliann Pezzullo <jump8621@gmail.com>
2021-02-15 14:27:59 -0500

    different route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask,
+#  render_template
 import cv2
 import numpy as np
 
@@ -6,8 +7,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+# def index():
+    # return render_template('index.html')
 
 
 if __name__ == "__main__":
