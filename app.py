5d4f86dab6ebab74186755941c5147a70122f186
Nadia Lashaw <nadia.lashaw@gmail.com>
2021-02-18 20:56:45 -0500

    test

--- app.py
+++ app.py
@@ -2,11 +2,53 @@ from flask import Flask, render_template
 #  render_template
 # import cv2
 # import Tensorflow
+import cv2
 import numpy as np
-import os
+import onnxruntime as ort
 
 
-import step_5_camera
+def center_crop(frame):
+    h, w, _ = frame.shape
+    start = abs(h - w) // 2
+    if h > w:
+        return frame[start: start + w]
+    return frame[:, start: start + h]
+
+
+def main():
+    # constants
+    index_to_letter = list('ABCDEFGHIKLMNOPQRSTUVWXY')
+    mean = 0.485 * 255.
+    std = 0.229 * 255.
+
+    # create runnable session with exported model
+    ort_session = ort.InferenceSession("signlanguage.onnx")
+
+    cap = cv2.VideoCapture(0)
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # preprocess data
+        frame = center_crop(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        x = cv2.resize(frame, (28, 28))
+        x = (x - mean) / std
+
+        x = x.reshape(1, 1, 28, 28).astype(np.float32)
+        y = ort_session.run(None, {'input': x})[0]
+
+        index = np.argmax(y, axis=1)
+        letter = index_to_letter[int(index)]
+
+        cv2.putText(frame, letter, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 2.0, (0, 255, 0), thickness=2)
+        cv2.imshow("Sign Language Translator", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 app = Flask(__name__)
 
@@ -26,7 +68,7 @@ def page1():
 
 @app.route('/test')
 def dynamic_page():
-    return step_5_camera.main()
+    return main()
 
 
 
